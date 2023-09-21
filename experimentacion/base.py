ddaf079a6eab315000740dad87b3e0adcb7cb036
Federico Arienti <fa.arienti@gmail.com>
2023-09-21 20:41:26 -0300

    add leer_dump

--- experimentacion/base.py
+++ experimentacion/base.py
@@ -1,28 +1,7 @@
 #!/usr/bin/env python3
 from scapy.all import *
-import numpy as np
 
 S1 = {}
-
-def informacion(ps):
-    i = -np.log2(ps)
-    return i
-
-def entropia(S): # S = [ps ... ]
-    N = sum(S.values())
-    e = np.sum([(k/N) * informacion(k/N) for k in S.values()])
-    return e
-
-def mostrar_informacion(S):
-    N = sum(S.values())
-    for s in S:
-        print(f"s={s} p={S[s]} i={informacion(S[s]/N)}")
-    print(f"e={entropia(S)}")
-
-def mostrar_fuente(S):
-    N = sum(S.values())
-    simbolos = sorted(S.items(), key=lambda x: -x[1])
-    print("\n".join([ "%s : %.5f" % (d,k/N) for d,k in simbolos ]))
 
 def callback(pkt):
     if pkt.haslayer(Ether):
@@ -30,13 +9,7 @@ def callback(pkt):
         tipo = IP if pkt.haslayer(IP) else Ether
         proto = pkt[Ether].type # El campo type del frame tiene el protocolo
         print(f"{pkt[tipo].src} -({dire}, {proto})-> {pkt[tipo].dst}")
-        s_i = (dire, proto) # Aca se define el simbolo de la fuente
-        if s_i not in S1:
-            S1[s_i] = 0.0
-        S1[s_i] += 1.0
 
 if __name__ == "__main__":
     
-    sniff(prn=callback, count=20000)
-    mostrar_fuente(S1)
-    mostrar_informacion(S1)
+    sniff(prn=callback, count=10000)
---
+++ experimentacion/leer_dump.py
@@ -0,0 +1,41 @@
+import re
+import numpy as np
+
+def informacion(ps):
+    i = -np.log2(ps)
+    return i
+
+def entropia(S): # S = [ps ... ]
+    N = sum(S.values())
+    e = np.sum([(k/N) * informacion(k/N) for k in S.values()])
+    return e
+
+def leer_dump(path):
+    S = {}
+    pattern = re.compile(r'-\((\w+), (\d+)\)->')
+    with open(path, 'r', encoding="utf-8") as file:
+        for line in file:
+            print(line[:-1])
+            match = pattern.search(line)
+            dire = match.group(1)
+            proto = match.group(2)
+            print(dire, proto)
+            s_i = (dire, proto) # Aca se define el simbolo de la fuente
+            if s_i not in S:
+                S[s_i] = 0.0
+            S[s_i] += 1.0
+
+    return S
+
+def mostrar_fuente(S):
+    N = sum(S.values())
+    simbolos = sorted(S.items(), key=lambda x: -x[1])
+    print("\n".join([ "%s, q: %d, p: %.5f, i: %.5f" % (d, k, k/N, informacion(k/N)) for d,k in simbolos ]))
+    
+if __name__ == "__main__":
+
+    path = "./fede-reposo.txt"
+    S = leer_dump(path)
+    print(S)
+    print(mostrar_fuente(S))
+    print(f"e={entropia(S)}")
