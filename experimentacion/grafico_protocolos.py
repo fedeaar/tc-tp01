b1b9849e7f4b6e09b59056dd180e1309fbb6205d
Federico Arienti <fa.arienti@gmail.com>
2023-09-21 21:02:21 -0300

    add cosas de graficos

--- experimentacion/grafico_protocolos.py
+++ experimentacion/grafico_protocolos.py
@@ -1,29 +1,40 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import leer_dump
 
-# Datos proporcionados
-data = {
-    ('UNICAST', 2048): 95430,
-    ('UNICAST', 2054): 4120,
-    ('UNICAST', 34525): 310,
-    ('BROADCAST', 2054): 130,
-    ('BROADCAST', 2048): 10,
-}
+def graficar(S1, S2, S3):
+    simbolos = set()
+    simbolos.update(S1.keys())
+    simbolos.update(S2.keys())
+    simbolos.update(S3.keys())
 
-# Separar los datos en listas de etiquetas y valores
-labels = [f'{key[0]}, {key[1]}' for key in data.keys()]
-values = list(data.values())
+    x = np.arange(len(simbolos))  # the label locations
+    width = 0.25  # the width of the bars
+    multiplier = 0
 
-# Crear el gráfico de barras
-plt.bar(labels, values)
+    fig, ax = plt.subplots(layout='constrained')
 
-# Etiquetas y título
-plt.xlabel('Tipo de Tráfico, Número')
-plt.ylabel('Valor')
-plt.title('Gráfico de Barras')
+    for attribute, measurement in simbolos.items():
+        offset = width * multiplier
+        rects = ax.bar(x + offset, measurement, width, label=attribute)
+        ax.bar_label(rects, padding=3)
+        multiplier += 1
 
-# Rotar las etiquetas del eje x para mejorar la legibilidad
-plt.xticks(rotation=45, ha="right")
-plt.yscale('log')
-# Mostrar el gráfico
-plt.tight_layout()
-plt.show()
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Length (mm)')
+    ax.set_title('Penguin attributes by species')
+    ax.set_xticks(x + width, simbolos)
+    ax.legend(loc='upper left', ncols=3)
+    ax.set_ylim(0, 250)
+
+    plt.show()
+    
+def graficar_reposo():
+    S1 = leer_dump.leer_dump("./out/fede-reposo.txt")    
+    S2 = leer_dump.leer_dump("./out/natan-reposo.txt")
+    S3 = leer_dump.leer_dump("./out/manu-reposo.txt")
+    graficar(S1, S2, S3)    
+
+if __name__ == "__main__":
+
+   graficar_reposo()
