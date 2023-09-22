4b3f7f33b48f1446a09201daaf87807fa73913aa
Federico Arienti <fa.arienti@gmail.com>
2023-09-21 21:21:13 -0300

    add graficos

--- experimentacion/grafico_protocolos.py
+++ experimentacion/grafico_protocolos.py
@@ -2,32 +2,45 @@ import matplotlib.pyplot as plt
 import numpy as np
 import leer_dump
 
-def graficar(S1, S2, S3):
-    simbolos = set()
-    simbolos.update(S1.keys())
-    simbolos.update(S2.keys())
-    simbolos.update(S3.keys())
+def graficar(data1, data2, data3):
+    # Crear un diccionario combinado con valores predeterminados de 0
+    combined_data = {}
+    for key in set(data1.keys()).union(data2.keys()).union(data3.keys()):
+        combined_data[key] = [x[key] / np.sum(list(x.values())) for x in [data1, data2, data3]]
 
-    x = np.arange(len(simbolos))  # the label locations
-    width = 0.25  # the width of the bars
-    multiplier = 0
+    # Separar los datos combinados en listas de etiquetas y valores
+    categories = [f'{key[0]}, {key[1]}' for key in combined_data.keys()]
+    values1, values2, values3 = zip(*combined_data.values())
 
-    fig, ax = plt.subplots(layout='constrained')
+    # Crear el gráfico de barras
+    plt.figure()
+    width = 0.2  # Ancho de las barras
 
-    for attribute, measurement in simbolos.items():
-        offset = width * multiplier
-        rects = ax.bar(x + offset, measurement, width, label=attribute)
-        ax.bar_label(rects, padding=3)
-        multiplier += 1
+    # Barras para el conjunto de datos 1
+    plt.bar(np.arange(len(categories)), values1, width, label='red 1')
 
-    # Add some text for labels, title and custom x-axis tick labels, etc.
-    ax.set_ylabel('Length (mm)')
-    ax.set_title('Penguin attributes by species')
-    ax.set_xticks(x + width, simbolos)
-    ax.legend(loc='upper left', ncols=3)
-    ax.set_ylim(0, 250)
+    # Barras para el red 2
+    plt.bar(np.arange(len(categories)) + width, values2, width, label='red 2')
 
+    # Barras para el red 3
+    plt.bar(np.arange(len(categories)) + 2 * width, values3, width, label='red 3')
+
+    # Configuración del eje x
+    plt.xticks(np.arange(len(categories)) + width, categories, rotation=45)
+
+    # Etiquetas y título
+    plt.xlabel('Simbolo')
+    plt.ylabel('Probabilidad')
+    plt.title('Gráfico de Barras de las tres redes')
+    plt.yscale('log')
+
+    # Leyenda
+    plt.legend()
+
+    # Mostrar el gráfico de barras combinado
+    plt.tight_layout()
     plt.show()
+
     
 def graficar_reposo():
     S1 = leer_dump.leer_dump("./out/fede-reposo.txt")    
@@ -35,6 +48,13 @@ def graficar_reposo():
     S3 = leer_dump.leer_dump("./out/manu-reposo.txt")
     graficar(S1, S2, S3)    
 
+def graficar_intenso():
+    S1 = leer_dump.leer_dump("./out/fede-intenso.txt")    
+    S2 = leer_dump.leer_dump("./out/natan-intenso.txt")
+    S3 = leer_dump.leer_dump("./out/manu-intenso.txt")
+    graficar(S1, S2, S3)    
+
 if __name__ == "__main__":
 
    graficar_reposo()
+   graficar_intenso()
